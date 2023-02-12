8d069cc3b7cd8a9f105918ab68b87131c96df84c
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-02-12 15:21:02 -0600

    added classes

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello");
+console.log("File is linked.");
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the name of the employee?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is the employee ID?',
+        name: 'employeeID'
+    },
+    {
+        type: 'input',
+        message: 'What is the email address of the employee?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is the office number of the employee?',
+        name: 'officeNumber'
+    },
+    {
+        type: 'input',
+        message: 'What is the Github username of the employee?',
+        name: 'github'
+    }
+])
